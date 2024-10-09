805e6c0f97664434079a1e9cb15432764b14370f
Daniel Hendricks <daniel@eyemeets.com>
2024-10-09 07:31:56 +0200

    chore: cleanup

--- src/index.ts
+++ src/index.ts
@@ -37,25 +37,9 @@ app.listen(port, localIp, async () => {
     directory: path.join(__dirname, 'routes')
   })
 
-  const prompt = 'Create a concise 20-word photorealistic image prompt focused on \"I want to learn about AI\" with a cinematic style. Include: real-world visuals and professional settings. Exclude: Ensure there is no text, sci-fi, or holographic elements in the image.'.trim()
-  // Works great w dale-3 -> const promptAlt = 'A serene classroom setting with books, a philosopher statue, and abstract thought bubbles, representing philosophy study.'
   console.log(`\n Express server running on http://${localIp}:${port}`)
-  console.log('OPENAI_API_KEY:', process.env.OPENAI_API_KEY)
-  //const url = await generateDalleImage(prompt)
-  //console.log('Url -> ', url)
-  return
 
-  // const url = await generateDalleImage(prompt)
-  // console.log('Url -> ', url)
-  /*const output = await replicate.run(
-    'stability-ai/stable-diffusion', // The model you want to use
-    {
-      input: {
-        prompt
-      }
-    }
-  )
-  console.log(output)*/
+  return
 })
 
 export default app
